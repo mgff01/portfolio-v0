8b96e5ebab4c86510de2b09f8261ee9c9145c968
Mauricio Filho <mauricio050607@gmail.com>
2026-01-26 01:23:48 -0300

    Update experience card icon source to Ibmec Logo

--- components/experience-card.tsx
+++ components/experience-card.tsx
@@ -25,7 +25,7 @@ export default function ExperienceCard({
       {/* Icon */}
       <div className="flex-shrink-0 w-10 h-10 rounded-lg bg-primary/10 flex items-center justify-center overflow-hidden">
         <Image
-          src={icon || "/placeholder.svg"}
+          src={icon || "/Ibmec Logo.svg"}
           alt={`${title} company logo`}
           width={40}
           height={40}
